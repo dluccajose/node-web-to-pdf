5284ee9a0dbbc35e8e1e4ad10332ed28824a340d
dluccajosetw <jose.dlucca@twgroup.cl>
2023-02-02 22:24:35 -0400

    upgrade express version

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
+  console.log(err)
   res.status(err.status || 500);
   res.render('error');
 });
