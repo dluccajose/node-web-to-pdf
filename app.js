20ff9f1817f6a68a053cb720b2fb86879de084b3
dluccajosetw <jose.dlucca@twgroup.cl>
2023-02-04 14:56:31 -0400

    refactor middleware

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var engine = require('express-handlebars').engine
 const resourcesRateLimiter = require('./middlewares/resourcesRateLimiter')
-console.log(resourcesRateLimiter)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -32,8 +31,6 @@ app.use(resourcesRateLimiter)
 // Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- middlewares/resourcesRateLimiter.js
+++ middlewares/resourcesRateLimiter.js
@@ -3,9 +3,8 @@ const os = require('os')
 module.exports = function (req, res, next) {
     const freeMemoryMB = os.freemem() / 1048576
 
-    if (freeMemoryMB < process.env.RATE_LIMITER_MEMORY) {
-        res.locals.error = 'En estos momentos nuestro sistemas se encuentran con mucho trafico, intente mas tarde.'
-        return res.render('error')
+    if (freeMemoryMB < process.env.RATE_LIMITER_MB_MEMORY) {
+        throw new Error('En estos momentos nuestro sistemas se encuentran con mucho trafico, intente mas tarde.')
     }
 
     next()
