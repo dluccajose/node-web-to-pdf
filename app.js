6fedde447cfce76f4ed86728329ce8844827faa0
dluccajosetw <jose.dlucca@twgroup.cl>
2023-02-04 14:45:09 -0400

    add resources limiter middleware

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var engine = require('express-handlebars').engine
+const resourcesRateLimiter = require('./middlewares/resourcesRateLimiter')
+console.log(resourcesRateLimiter)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,15 +18,22 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'storage/public')));
 
+
+// Middlewares
+app.use(resourcesRateLimiter)
+
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
